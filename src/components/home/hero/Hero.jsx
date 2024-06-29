15c07e2e22424f303eb0a2d4d266d9e686058947
Salmon <salmonlearner34@gmail.com>
2024-06-29 09:04:27 +0500

    size

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -9,26 +9,16 @@ import "./Hero.scss";
 const Hero = () => {
   const images = [Hero1, Hero2, Hero3, Hero4, Hero5];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [loading, setLoading] = useState(true); // State for loading
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 3000);
 
-    // Simulate loading time (adjust timing as necessary)
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
     return () => {
       clearInterval(interval);
     };
   }, [images.length]);
-
-  if (loading) {
-    return <div className="loader">Loading...</div>;
-  }
 
   return (
     <>
@@ -45,8 +35,8 @@ const Hero = () => {
         <div className="hero-overlay"></div>
         <div className="hero-content">
           <h1>
-            Sizning Uyingiz Bizning Ustalarimiz Qo'lida Yangi Ko'rinishga Ega
-            Bo'ladi
+            SIZNING UYINGIZ BIZNING USTALARIMIZ QO'LIDA YANGI KO'RINISHGA EGA
+            BO'LADI
           </h1>
           <p>
             Eng ishonchli va malakali ustalar, uyingizdagi barcha ta'mirlash va
